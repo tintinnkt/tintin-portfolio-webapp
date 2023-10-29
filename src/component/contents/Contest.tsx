2b3861fa2c77e44bddde5ad54ca6278f60a403b4
Krittin Tragunejindarat <itin752@gmail.com>
2023-10-29 19:00:14 +0700

    posn + KU01 + readtoggle

--- src/component/contents/Contest.tsx
+++ src/component/contents/Contest.tsx
@@ -1,29 +1,83 @@
-import React from 'react'
+import React, { useState } from 'react'
 import style from '../../style/contentStyle/Contest.module.css'
 import posn from '../../assets/picDerectory/posn.json'
 //component
 import ContentHeader from '../ContentHeader'
 import PictureSlide from './PictureSlide'
+
 type Props = {}
 
 const Contest = (props: Props) => {
+
+    const [showMore, setShowMore] = useState(false);
+
+    const toggleReadMore = () => {
+        setShowMore(!showMore);
+    };
+    const [showMorePOSN, setShowMorePOSN] = useState(false);
+    const toggleReadMorePOSN = () => {
+        setShowMorePOSN(!showMorePOSN);
+    };
+
     return (
         <div className={style.container}>
             <ContentHeader name='Contests' componentProp={null} />
             <main className={style.content}>
+                <div className={style.topicContest}>POSN Camp 1 && Camp 2</div>
                 <div className={style.posn}>
-                    <div className={style.picCon}>
-                    <PictureSlide images={[...posn.posn1, ...posn.posn2]} style={{height:'400px'}}/>
+                    <div className={style.detail}>
+                        <div className={style.picCon}>
+                            <PictureSlide images={[...posn.posn2, ...posn.posn1,]} style={{ height: '400px' }} />
+                        </div>
+                        <p>Through my participation in POSN camp1 and POSN camp2, I have gained a strong foundation in programming concepts and techniques. In the Camp 1st, I learned the basics of C programming, including variable types, operators, conditional statements, loops, arrays, strings, functions, pointers, and file management. This foundational knowledge laid the groundwork for my subsequent exploration of C++ programming in the Camp 2nd. There, I delved into {!showMorePOSN?<span className={style.show}onClick= {toggleReadMorePOSN}>...Read More</span>:
+                            <span>the intricacies of data structures, such as linked lists, stacks, queues, heaps, and binary search trees, and gained proficiency in selecting appropriate algorithms for various problem-solving scenarios. My exposure to algorithms encompassed brute force algorithms, recursion, dynamic programming, greedy algorithms, Dijkstra's algorithm, and Big-O analysis of algorithms. These intensive learning experiences have equipped me with a comprehensive understanding of programming principles and practices, enabling me to confidently tackle complex programming challenges.<span className={style.show} onClick={toggleReadMorePOSN}>Read Less...</span></span>}</p>
                     </div>
                     <div className={style.certificate}>
-                        <img src="src/assets/contestPics/POSN/certificate/certificate-posn1.png" alt="posn1" />
-                        <img src="src/assets/contestPics/POSN/certificate/certificate-posn2.png" alt="posn2" />
+                        <div className={style.picCon}>
+                            <img src="src/assets/contestPics/POSN/certificate/certificate-posn1.png" alt="posn1" />
+                            <div className={style.description}>เมื่อวันที่ 6-22 ตุลาคม พ.ศ.2565 <br />
+                                ศูนย์โอลิมปิกวิชาการ สอวน. ค่าย 1 วิชาคอมพิวเตอร์  <br />ปีการศึกษา 2565
+                                โรงเรียนสตรีวัดมหาพฤฒาราม ในพระบรมราชินูปถัมภ์</div>
+                        </div>
+                        <div className={style.picCon}>
+                            <img src="src/assets/contestPics/POSN/certificate/certificate-posn2.png" alt="posn2" />
+                            <div className={style.description}>เมื่อวันที่ 20 มีนาคม - 5 เมษายน พ.ศ.2566 <br />
+                                โครงการส่งเสริมโอลิมปิกวิชาการและพัฒนามาตรฐานวิทยาศาสตร์คณิตศาสตร์
+                                (สอวน.) <br />วิชาคอมพิวเตอร์ ค่าย 2 ปีการศึกษา 2565
+                                คณะวิทยาศาสตร์ มหาวิทยาลัยเกษตรศาสตร์</div>
+                        </div>
                     </div>
+                    <div className={style.detail}></div>
                     <p></p>
                     <p></p>
 
                 </div>
+
+            </main>
+            <main className={style.content}>
+                <div className={style.topicContest}>KU01 Programming Contest</div>
                 <div className={style.ku01}>
+                    <div className={style.mainContent}>
+                        <div className={style.certificate}>
+                            <div className={style.picCon}><img src="src/assets/contestPics/KU01/ku01_certificate.png" alt="" />
+                                <div className={style.description}>
+                                    KU01 การทดสอบความสามารถในการแก้ปัญหาโดยการเขียนโปรแกรม <br />เมื่อวันที่ 17 ธันวาคม พ.ศ.2565 <br />
+                                    คณะวิศวะกรรมศาสตร์ ณ มหาวิทยาลัยเกษตรศาสตร์</div></div></div>
+                        <div className={style.detail}>
+                            <p>
+                            <p>On December 17, 2023, I participated in the KU01 Programming Contest, hosted by the Department of Computer Engineering at Kasetsart University. I trained myself with C++ and passed the online round to reach the onsite round at Kasetsart University.</p>                 <p>At the onsite round, I met with fellow students who are passionate about solving problems with programming. We exchanged ideas and experiences, and I learned a lot about solving different types of programming problems. Even though I did not reach the final round, I am proud to have participated in the contest and gained valuable experience.{!showMore?<span className={style.show}onClick= {toggleReadMore}>...Read More</span>:null}</p>
+                            {showMore ? <><p>In addition to the contest, I also attended a guidance hosted by the Department of Computer Engineering. The seminar was very helpful for me to plan my future studies and learn more about the field of computer science.</p>
+                            <p>At the end of the contest, I had the opportunity to listen to a presentation of research projects by graduate students. The presentation inspired me to pursue further studies and develop my programming problem-solving skills.<span className={style.show} onClick={toggleReadMore}>Read Less...</span></p></>: null}
+                            </p>
+                        </div>
+                    </div>
+                    <div className={style.subContent}>
+                        <div className={style.picCon}>
+                            <img className={style.insideImg} src="src/assets/contestPics/KU01/image23412342.png" alt="" />
+                            <img src="https://media.discordapp.net/attachments/1138722166758707321/1168128756712489021/IMG_20221217_140738.jpg?ex=6550a3ae&is=653e2eae&hm=b609c1893229a7d1bd620f097f718d77960548721d12e6013dd8520f1f4681dc&=&width=676&height=507" alt="" />
+                            <div className={style.description}>Post-competition career guidance and presentation of research projects by students from the Department of Computer Engineering at Kasetsart University</div>
+                        </div>
+                    </div>
 
                 </div>
 
--- src/component/contents/project/fiManagement.tsx
+++ src/component/contents/project/fiManagement.tsx
@@ -1,46 +1,71 @@
-import React from 'react'
+import React,{useState} from 'react'
 import style from '../../../style/contentStyle/projectContent.module.css'
 import imgData from '../../../assets/picDerectory/fi-management.json'
 
 import PictureSlide from '../PictureSlide'
 type Props = {}
 
-const FiManagement = (props: Props) => {
+const FiManagement = (props: Props) =>{
+  const [showMore, setShowMore] = useState(false);
+
+  const toggleReadMore = () => {
+    setShowMore(!showMore);
+  };
+  
   return (
     <section className={style.ProjectContent} id='fi-management-webapp'>
-            <div className={style.detail}>
-              <div className={style.ProjectHeader}> <span>1</span> Money Management Web App</div>
-              <div className={style.description}>
-                <p className={style.text}>Lorem, distinctio totam.  sed. Est alias, earum accusamus recusandae ea aut deserunt suscipit id beatae iure. Sequi dolore aliquam hic voluptatibus labore ab ex natus non velit quis mollitia quos molestiae dolores ut, at iste magnam, itaque aliquid possimus nulla vel accusantium. Reiciendis porro pariatur molestiae soluta veniam quasi laboriosam quo dignissimos voluptatem alias aut iste eligendi, vitae libero, commodi cumque obcaecati incidunt. Est, ut ad.</p>
-              </div>
-              <div className={style.learn}>
-                <div className={style.header}>Things I learned from this project</div>
-                <ul>
-                  <li>Planning and executing web application development</li>
-                  <li>Creating folder structures for web applications</li>
-                  <li>Creating React components for reusable components</li>
-                  <li>Using CSS modules to reduce problems caused by
-                    <br />duplicate class names between components</li>
-                  <li>Connecting a web application to a database: Cloud Firestore</li>
-                  <li>Performing CRUD (Create, read, update, delete) operations on data</li>
-                  <li>Resolving conflicts caused by simultaneous code changes</li>
-                  <li>Deploying a project to Firebase Hosting</li>
-                </ul></div>
-              <div className={style.connectContainer}>
-                <div>Inspect Sorce Code and Documentation here</div>
-                <div className={style.connect}>
-                  <a href="https://github.com/TintinnKT/fi-management-webapp"><img src="https://cdn-icons-png.flaticon.com/512/1051/1051275.png" alt="Github" /></a>
-                  <a href="https://drive.google.com/drive/u/0/folders/1YaoHbU25biHyA-tbUu-g1I-eJT0A0Nzg"><img src="https://cdn-icons-png.flaticon.com/512/3073/3073412.png " alt="Google Drive" /></a>
-                </div>
-              </div>
+      <div className={style.detail}>
+        <div className={style.ProjectHeader}> <span>1</span> Money Management Web App</div>
+          <p className={style.note}>My First Web App Project</p>
+        <div className={style.description}>
+
+          <p className={style.text}>As a high school student that interested in web development, my first project was a web application. It was a challenging but rewarding experience.</p>
+
+          <p className={style.text}>I began by learning HTML, CSS, and JavaScript, drawing on my prior knowledge of C and C++ for a quicker grasp of the concepts. I supplemented my learning with YouTube tutorials and documentation.</p>
+          
+          <p className={style.text}>Once I had a solid foundation, I used React and Vite.js to build the web app, designing the user interface in Figma.{!showMore?<span className={style.show}onClick= {toggleReadMore}>...Read More</span>:null}</p>
+
+          {showMore ? <><p className={style.text}>I collaborated with a friend, focusing on the front-end and dummy data. Along the way, I deepened my understanding of React Router DOM and related technologies.</p>
+
+          <p className={style.text}>Towards the end, I tackled Google login integration and Firestore Firebase for database management, with the help of online resources and my friend.</p>
+
+          <p className={style.text}>Our web app is a Money Management App built with React.js, enabling users to track their finances. We hosted it on Firebase for accessibility.</p>
+
+          <p className={style.text}>This project taught me valuable lessons in planning, organizing code, creating reusable components, using CSS modules, connecting to Firestore, resolving conflicts, and deploying to Firebase Hosting.</p>
+
+          <p className={style.text}>I'm proud of my accomplishments and look forward to continuing my growth as a software engineer.<span className={style.show} onClick={toggleReadMore}>Read Less...</span>
+            </p></>: null}
+        </div>
+        <div className={style.learn}>
+          <div className={style.header}>Things I learned from this project</div>
+          <ul>
+            <li>Strong foundation building with HTML, CSS, and JavaScript</li>
+            <li>Effective utilization of React and its ecosystem</li>
+            <li>Collaboration and teamwork with fellow developers</li>
+            <li>Deeper proficiency in web technologies and React Router DOM</li>
+            <li>Integration of Google login and Firestore Firebase for data management</li>
+            <li>Deployment of a web application on Firebase</li>
+            <li>Structural organization and folder management</li>
+            <li>Efficient CSS management using CSS modules</li>
+            <li>Database operations, including CRUD functions</li>
+            <li>Code conflict resolution and effective use of version control</li>
+          </ul></div>
+
+        <div className={style.connectContainer}>
+          <div>Inspect Sorce Code and Documentation here</div>
+          <div className={style.connect}>
+            <a href="https://github.com/TintinnKT/fi-management-webapp"><img src="https://cdn-icons-png.flaticon.com/512/1051/1051275.png" alt="Github" /></a>
+            <a href="https://drive.google.com/drive/u/0/folders/1YaoHbU25biHyA-tbUu-g1I-eJT0A0Nzg"><img src="https://cdn-icons-png.flaticon.com/512/3073/3073412.png " alt="Google Drive" /></a>
+          </div>
+        </div>
 
 
-            </div>
-            <div className={style.pics}><PictureSlide
-              images={imgData}
-              style={{ width: '335px', height: '641px' }}
-            /></div>
-          </section>
+      </div>
+      <div className={style.pics}><PictureSlide
+        images={imgData}
+        style={{ width: '335px', height: '641px' }}
+      /></div>
+    </section>
   )
 }
 
